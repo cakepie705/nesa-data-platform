ff1ad840a5bdbba0016f7b376f564a4514bfd7a9
guitarc123 <philip9chaplin@hotmail.com>
2026-07-16 08:56:31 +0100

    Update project files

---
+++ sql/schema/001_create_schemas.sql
@@ -0,0 +1,13 @@
+-- ===========================================
+-- NESA Analytics Platform
+-- Sprint 2
+-- Create Database Schemas
+-- ===========================================
+
+CREATE SCHEMA IF NOT EXISTS raw;
+
+CREATE SCHEMA IF NOT EXISTS staging;
+
+CREATE SCHEMA IF NOT EXISTS warehouse;
+
+CREATE SCHEMA IF NOT EXISTS analytics;
